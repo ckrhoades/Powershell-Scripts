8b6e9be1e58161861dcb2ddf10ab09344505fb18
mistervandal <41246414+mistervandal@users.noreply.github.com>
2019-06-21 17:49:07 -0700

    Expression Matching Script
    Does basic expression matching against a group of files.

--- Search files for expression.ps1
+++ Search files for expression.ps1
@@ -1,22 +1,19 @@
 ﻿#Chris's basic script for simple expression matching.
 
 #This is the expression to be matched, currently it's format is XXXX-XXXX-XXXX-XXXX
-#This can be changed to easily do social security numbers or othe items with similiar formats
+#This can be changed to easily do social security numbers or othe items with similiar formats or anything really
 $var_match_exp = "[0-9][0-9][0-9][0-9]-[0-9][0-9][0-9][0-9]-[0-9][0-9][0-9][0-9]-[0-9][0-9][0-9][0-9]"
 #This takes the contents of the current directory and adds them to the list
 #Adding -recurse and -file to this makes it go through recursivly and adding only files.
 
-#$var_group = get-childitem -Recurse -File | ?{$_.extension -EQ ".txt"} | %(write-host $_.name)
 $var_group = (get-childitem -Recurse -file).Name
 
 
 #The foreach statement below runs through the list and checks the files for the expression.  If there's a match to the expression it will return a true.
 #the process grbs the itme and pushes it into a variable, then expression matches that against the varaible.
-#if the expression is matched, the return is true.
-#Added toupper() to this so that expressions such as PASSWORD could be searched by changing the var_match_exp expression.
 foreach($item in $var_group) {
  $var_filetest = get-content $item
  $var_result = $var_filetest.ToUpper() -match $var_match_exp
     if ($var_result -eq $true)
-        {write-host $item "contains possbile cc data" }
+        {write-host $item "contains possbile data" }
 }
