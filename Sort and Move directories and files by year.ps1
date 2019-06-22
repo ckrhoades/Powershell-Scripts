e9c726b244c170042d7bc30dbe09e13e456069f6
mistervandal <41246414+mistervandal@users.noreply.github.com>
2019-06-21 17:45:28 -0700

    Update Sort and Move directories and files by year.ps1
    Cleaned up and most o the comments,

--- Sort and Move directories and files by year.ps1
+++ Sort and Move directories and files by year.ps1
@@ -1,12 +1,5 @@
-﻿#Below builds an array based on file dates year, this builds the list for the loop.
-
-$var_year = (Get-ChildItem).LastWriteTime.Year |  select -uniq
-
-#This is the variable for the current year, if files are in this year, it won't move those files.
-
+$var_year = (Get-ChildItem).LastWriteTime.Year |  select -unique
 $var_current_year = (Get-Date).Year
-
-#The Loop is here, this is what moves the files.
 
 foreach($y in $var_year){
 
@@ -14,22 +7,15 @@ foreach($y in $var_year){
 
   if ($y -ne $var_current_year){  
 
-#Check to see if directory exists, based on year, if not create it.
-
     if(!(Test-Path -Path "_$y" -PathType Container))
     {
         mkdir "_$y"
     }    
 
-#Create the array, ie list of files to be moved.  
-
     $var_child = get-childitem | where-object{$_.LastWriteTime.Year -eq "$y" -and $_.name -notlike "_*"}
-
-#Move the files
 
     foreach($i in $var_child){
     Move-Item -Path $i -Destination "_$y"   
     }
   }
 }
-
