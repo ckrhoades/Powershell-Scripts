b184dde9c4c21be132c19e0328550893ca7decce
mistervandal <41246414+mistervandal@users.noreply.github.com>
2019-06-21 17:59:29 -0700

    Update and sort files only by year last accessed

--- Sort and Move Files By year.ps1
+++ Sort and Move Files By year.ps1
@@ -1,31 +1,18 @@
-﻿#Below builds an array based on file dates year, this builds the list for the loop.
 
 $var_year = (Get-ChildItem -file).LastWriteTime.Year |  select -uniq
-
-#This is the variable for the current year, if files are in this year, it won't move those files.
-
 $var_current_year = (Get-Date).Year
-
-#The Loop is here, this is what moves the files.
 
 foreach($y in $var_year){
 
-#If statement below byapasses the current year, making sure only old files get moved.
-
   if ($y -ne $var_current_year){  
-
-#Check to see if directory exists, based on year, if not create it.
 
     if(!(Test-Path -Path "_$y" -PathType Container))
     {
         mkdir "_$y"
     }    
 
-#Create the array, ie list of files to be moved.  
 
     $var_child = get-childitem -file | where-object{$_.LastWriteTime.Year -eq "$y"}
-
-#Move the files
 
     foreach($i in $var_child){
     Move-Item -Path $i -Destination "_$y"   
