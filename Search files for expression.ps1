4d36bd692c047fa9a544e193c6b0507d35677f70
ckrhoades <52089333+ckrhoades@users.noreply.github.com>
2019-06-23 12:23:37 -0700

    Update Search files for expression.ps1

--- Search files for expression.ps1
+++ Search files for expression.ps1
@@ -6,7 +6,7 @@ $var_match_exp = "[0-9][0-9][0-9][0-9]-[0-9][0-9][0-9][0-9]-[0-9][0-9][0-9][0-9]
 #This takes the contents of the current directory and adds them to the list
 #Adding -recurse and -file to this makes it go through recursivly and adding only files.
 
-$var_group = (get-childitem -Recurse -file).Name
+$var_group = (get-childitem -file).Name
 
 
 #The foreach statement below runs through the list and checks the files for the expression.  If there's a match to the expression it will return a true.
@@ -14,6 +14,6 @@ $var_group = (get-childitem -Recurse -file).Name
 foreach($item in $var_group) {
  $var_filetest = get-content $item
  $var_result = $var_filetest.ToUpper() -match $var_match_exp
-    if ($var_result -eq $true)
+    if ($var_result)
         {write-host $item "contains possbile data" }
 }
